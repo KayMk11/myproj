fa4958d19921d341c1943cdfbb2c5d8f3bdc9faa
Gitesh24398 <narkhedegitesh@gmail.com>
2021-07-09 09:02:19 +0530

    Added: Publisher fuctionality

--- src/app/Publisher/displayAll-publishers/display-all.component.ts
+++ src/app/Publisher/displayAll-publishers/display-all.component.ts
@@ -1,5 +1,7 @@
-import { FormGroup } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { PublisherService } from './../../Services/publisher.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Publisher } from 'src/app/models/publisher.model';
 
 @Component({
@@ -8,11 +10,102 @@ import { Publisher } from 'src/app/models/publisher.model';
   styleUrls: ['./display-all.component.css']
 })
 export class DisplayAllComponent implements OnInit {
-  searchPublisher:FormGroup;
-  publishers:Publisher[];
-  constructor() { }
+  searchPublisher : Publisher;
+  publishers: Publisher[];
+  publisher: Publisher;
+  updatePublisher:FormGroup;
+  deleteMsg:string = '';
+  allPublisher = false;
+  isSearched = false
+  
+  @ViewChild('closebutton') closebutton: { nativeElement: { click: () => void; }; };
+  constructor(private publisherservie:PublisherService) {
+    this.publisherservie.getAllPublishers().subscribe(data =>{
+      console.log(data);
+      this.publishers = data;
+  })
+   }
 
   ngOnInit(): void {
   }
 
+  onClickDelete(publisherId:number){
+    this.publisherservie.deletePublisher(publisherId)
+    .subscribe(data=>{
+      this.deleteMsg = 'Publisher Deleted Successfully!!';
+      this.publisherservie.getAllPublishers().subscribe(data=>{
+        this.publishers = data;
+      })
+    },error=>{
+      this.deleteMsg = error
+    });
+  }
+  publisherUpdateForm = new FormGroup({
+    publisherId: new FormControl({value:'', disabled:true}),
+    publisherName: new FormControl('', [Validators.required]),
+    contactNo: new FormControl('', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    address1: new FormControl('', [Validators.required]),
+    address2: new FormControl('', [Validators.required]),
+    city: new FormControl('', [Validators.required]),
+    state: new FormControl('', [Validators.required]),
+    pincode: new FormControl('', [Validators.required])
+  });
+
+  onClickUpdate(publisherId:number){
+    this.publisherservie.getPublisherById(publisherId)
+    .subscribe(data=>{
+      this.publisher = data;
+      console.log(this.publisher);
+      this.newUpdateForm();
+    })
+  }
+
+  newUpdateForm(){
+    this.publisherUpdateForm.setValue({
+      publisherId : this.publisher.publisherId,
+      publisherName : this.publisher.publisherName,
+      email: this.publisher.email,
+      contactNo : this.publisher.contactNo,
+      address1 : this.publisher.address1,
+      address2 : this.publisher.address2,
+      city : this.publisher.city,
+      state : this.publisher.state,
+      pincode : this.publisher.pincode
+    });
+  }
+
+  onSubmit(){
+    let publisher = new Publisher();
+    publisher.publisherId = this.publisherUpdateForm.getRawValue().publisherId;
+    publisher.publisherName = this.publisherUpdateForm.value.publisherName;
+    publisher.contactNo = this.publisherUpdateForm.value.contactNo;
+    publisher.address1 = this.publisherUpdateForm.value.address1;
+    publisher.address2 = this.publisherUpdateForm.value.address2;
+    publisher.email = this.publisherUpdateForm.value.email;
+    publisher.city = this.publisherUpdateForm.value.city;
+    publisher.state = this.publisherUpdateForm.value.state;
+    publisher.pincode = this.publisherUpdateForm.value.pincode;
+
+    this.publisherservie.updatePublisher(publisher).subscribe(data=>{
+      this.publisherservie.getAllPublishers().subscribe(e=>{
+       this.publishers = e;
+      })
+    },
+    error=> console.log(error));
+
+  }
+
+  publisherSearchForm=new FormGroup({
+    publisherId :new FormControl('',[Validators.required])});
+
+    onSearch(){
+      this.publisherservie.getPublisherById(this.publisherSearchForm.value.publisherId).subscribe(
+        data=>{
+          this.allPublisher=true;
+          this.isSearched=true;
+          this.searchPublisher = data;
+          console.log(this.publisher);
+        },error=>{})
+    }
 }
---
+++ src/app/Services/publisher.service.ts
@@ -0,0 +1,51 @@
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Publisher } from '../models/publisher.model';
+const headers = new HttpHeaders().set('Access-Control-Allow-Origin', '*');
+@Injectable({
+  providedIn: 'root'
+})
+export class PublisherService {
+  private baseUrl = 'http://localhost:8080/Publisher';
+  constructor(private httpclient:HttpClient) { }
+
+  addPublisher(publisher:Publisher): Observable<Publisher>{
+    console.log(publisher)
+    return this.httpclient.post<Publisher>(`${this.baseUrl}/add`, publisher,{headers})
+  }
+
+  getAllPublishers(){
+    return this.httpclient.get<Publisher[]>(`${this.baseUrl}/all`)
+    .pipe(catchError(this.handleError));
+  }
+
+  deletePublisher(publisherId: number):Observable<{}>{
+    return this.httpclient.delete(`${this.baseUrl}/delete/${publisherId}`)
+    .pipe(catchError(this.handleError));
+  }
+
+  updatePublisher(publisher:Publisher){
+    console.log(publisher);
+    return this.httpclient.put(`${this.baseUrl}/update`, publisher,{headers})
+    .pipe(catchError(this.handleError));
+  }
+
+  getPublisherById(publisherId: number){
+    return this.httpclient.get<Publisher>(`${this.baseUrl}/${publisherId}`,{headers})
+    .pipe(catchError(this.handleError));
+  }
+
+  private handleError(httpError: HttpErrorResponse) {
+    if (httpError.error instanceof ErrorEvent) {
+      console.error('An error occurred:', httpError.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${httpError.status}, ` +
+        `body was: ${httpError.error}`);
+    }
+    return throwError('Something bad happened; Give correct data');
+  }
+
+}
